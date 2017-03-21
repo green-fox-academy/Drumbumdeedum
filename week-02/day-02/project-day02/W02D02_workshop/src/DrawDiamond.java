2c4d9530eeddea6cca50ed9274f4562fcbcfcd89
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-21 22:28:20 +0100

    add DrawDiamond.java excersize

---
+++ week-02/day-02/project-day02/W02D02_workshop/src/DrawDiamond.java
@@ -0,0 +1,33 @@
+/**
+ * Created by danielszalay on 2017. 03. 21..
+ */
+
+import java.util.Scanner;
+
+public class DrawDiamond {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter a number: ");
+    int userInt = scanner.nextInt();
+    int a = 1;
+    String star = "*";
+    String empty = " ";
+    while (a <= userInt / 2 + 1) {
+      System.out.println(
+              (new String(new char[(userInt+1)/2-a]).replace("\0", empty))
+              +(new String(new char[a]).replace("\0", star))
+              +(new String(new char[a-1]).replace("\0", star)));
+      a +=1;
+    }
+    a = 1;
+    while (a <= userInt /2) {
+      System.out.println(
+              (new String(new char[a]).replace("\0", empty))
+              + (new String(new char[(userInt+1)/2-a]).replace("\0", star))
+              + (new String(new char[userInt / 2 - a]).replace("\0", star))
+              );
+      a += 1;
+    }
+  }
+}
+
