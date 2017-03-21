deb41c958a9dc911dc52d80dffd70ceb6785fdbb
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-21 16:04:31 +0100

    add AnimalsAndLegs java excersize

---
+++ week-02/day-02/project-day02/W02D02_workshop/src/AnimalsAndLegs.java
@@ -0,0 +1,22 @@
+/**
+ * Created by danielszalay on 2017. 03. 21..
+ */
+import java.util.Scanner;
+
+public class AnimalsAndLegs {
+  public static void main(String[] args) {
+    // Write a program that asks for two integers
+    // The first represents the number of chickens the farmer has
+    // The second represents the number of pigs the farmer has
+    // It should print how many legs all the animals have
+    System.out.println("How many chicken does the farmer have?");
+    Scanner scanner = new Scanner(System.in);
+    int numberOfChicken = scanner.nextInt();
+    System.out.println("How many pigs does the farmer have?");
+    int numberOfPigs = scanner.nextInt();
+    int numberOfChickenLegs = numberOfChicken * 2;
+    int numberOfPigLegs = numberOfPigs * 4;
+    int totalLegs = numberOfChickenLegs + numberOfPigLegs;
+    System.out.println("The farmers animals have " + totalLegs + " legs in total! Impressive!");
+  }
+}
