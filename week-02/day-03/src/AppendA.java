adac3d2b1b12a7ae86339ae239bdd217dc266172
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-22 12:16:39 +0100

    add AppendA.java excersize

---
+++ week-02/day-03/src/AppendA.java
@@ -0,0 +1,23 @@
+/**
+ * Created by danielszalay on 2017. 03. 22..
+ */
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `am` and assign the value `kuty` to it
+// - Write a function called `appendA` that gets a string as an input
+//   and appends an 'a' character to its end
+// - Print the result of `appendA(am)`
+
+public class AppendA {
+
+  public static void main(String[] args) {
+    System.out.println(appendA(am));
+  }
+
+  static String am = "kuty";
+
+  public static String appendA (String input) {
+    return (input + "a");
+  }
+}
