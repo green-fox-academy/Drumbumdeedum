9ca721b0219403b94b4268c7c9515cc455b11b42
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-22 08:32:27 +0100

    add ParametricAverage.java excersize

---
+++ week-02/day-02/project-day02/W02D02_workshop/src/ParametricAverage.java
@@ -0,0 +1,28 @@
+/**
+ * Created by danielszalay on 2017. 03. 22..
+ */
+// Write a program that asks for a number.
+// It would ask this many times to enter an integer,
+// if all the integers are entered, it should print the sum and average of these
+// integers like:
+//
+// Sum: 22, Average: 4.4
+
+import java.util.Scanner;
+
+public class ParametricAverage {
+  public static void main(String[] args) {
+    int random = (int)(Math.random()* 15 + 5);
+    int a = random;
+    int total = 0;
+    int userInt = 0;
+    while (a > 0) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Please enter a number!");
+      userInt = scanner.nextInt();
+      total = total + userInt;
+    a -= 1;
+    }
+    System.out.println("Sum: " + total + " Average: " + total/random);
+  }
+}
