ffdee63ca4a9276da73a700bb2312e8f9352a8f4
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-22 15:25:43 +0100

    add PrintAll.java excersize

---
+++ week-02/day-03/src/PrintAll.java
@@ -0,0 +1,16 @@
+/**
+ * Created by danielszalay on 2017. 03. 22..
+ */
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
+
+import java.util.Arrays;
+
+public class PrintAll {
+  public static void main(String[] args) {
+    int[] af = {4, 5, 6, 7};
+
+    System.out.println(Arrays.toString(af));
+  }
+}
