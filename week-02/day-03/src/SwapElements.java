c8da3a577b70956db916940a5264429b3e6e66ad
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-22 17:15:36 +0100

    add SwapElements.java excersize

---
+++ week-02/day-03/src/SwapElements.java
@@ -0,0 +1,22 @@
+/**
+ * Created by danielszalay on 2017. 03. 22..
+ */
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
+import java.util.Arrays;
+
+
+public class SwapElements {
+  public static void main(String[] args) {
+    String[] abc = {"first", "second", "third"};
+
+    String swapper = "";
+
+    swapper = abc[0];
+    abc[0] = abc[2];
+    abc[2] = swapper;
+
+    System.out.println(Arrays.toString(abc));
+  }
+}
