371f4797f0fa732aaf47bc8ed2f8501281c9b22a
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-22 07:39:24 +0100

    add DrawDiagonal.java excersize

---
+++ week-02/day-02/project-day02/W02D02_workshop/src/DrawDiagonal.java
@@ -0,0 +1,38 @@
+/**
+ * Created by danielszalay on 2017. 03. 22..
+ */
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%%%
+// %%    %
+// % %   %
+// %  %  %
+// %   % %
+// %    %%
+// %%%%%%%
+//
+// The square should have as many lines as the number was
+
+import java.util.Scanner;
+
+public class DrawDiagonal {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter a number: ");
+    int userInt = scanner.nextInt();
+    int a = 1;
+    System.out.println(new String(new char[userInt]).replace("\0", "*"));
+    while (a <= userInt-2) {
+      System.out.println(
+              ("*") +
+              (new String(new char[a-1]).replace("\0", " ")) +
+              ("*") +
+              (new String(new char[userInt - 2 - a]).replace("\0", " ")) +
+              ("*"));
+      a += 1;
+    }
+    System.out.println(new String(new char[userInt]).replace("\0", "*"));
+  }
+}
