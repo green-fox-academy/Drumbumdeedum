13e83ad8a84d550d673071de879bdc556d5bbddc
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-22 16:10:32 +0100

    add DiagonalMatrix.java excersize

---
+++ week-02/day-03/src/DiagonalMatrix.java
@@ -0,0 +1,33 @@
+/**
+ * Created by danielszalay on 2017. 03. 22..
+ */
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+import java.util.Scanner;
+
+public class DiagonalMatrix {
+  public static void main(String[] args) {
+
+
+    int size = 8;
+    int [][] matrix = new int [size][size];
+
+    for (int x = 0; x < size; x++) {
+      for (int y = 0; y < size; y++) {
+        if (x == y) {
+          System.out.printf("1 ", matrix[x][y]);
+        } else {
+          System.out.printf("0 ", matrix[x][y]);
+        }
+      }
+      System.out.println();
+    }
+  }
+}
