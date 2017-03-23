74c781714dc97a47dde31ae5e6cc7b6ca5a2af19
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-23 17:45:42 +0100

    add Matchaking and AppendLetter excersizes

---
+++ week-02/day-04/W02D04_excersizes/src/AppendLetter.java
@@ -0,0 +1,20 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+import java.util.*;
+
+/**
+ * Created by danielszalay on 2017. 03. 23..
+ */
+// Add "a" to every string in the far list.
+
+public class AppendLetter{
+  public static void main(String... args){
+    ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+
+    for (int i = 0; i < far.size(); i++) {
+      far.add(i, far.get(i) + "a");
+      far.remove(i+1);
+    }
+
+    System.out.println(far);
+  }
+}
