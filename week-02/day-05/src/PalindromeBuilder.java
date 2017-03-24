d7f73a78b1d9a6247fec619930403f5e2d5795e6
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-24 17:04:08 +0100

    updated AdagramChecker, added PalindromeBuilder excersize

---
+++ week-02/day-05/src/PalindromeBuilder.java
@@ -0,0 +1,21 @@
+/**
+ * Created by danielszalay on 2017. 03. 24..
+ */
+
+import java.util.Scanner;
+
+public class PalindromeBuilder {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+
+    System.out.println("Please enter a word:");
+    String userInput = scanner.nextLine();
+
+    createPalindrome(userInput);
+  }
+
+  public static void createPalindrome(String word) {
+
+
+  }
+}
