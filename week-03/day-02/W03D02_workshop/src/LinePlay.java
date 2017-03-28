a63a71338a6fa5acbfa2e5ef6aea1776e3fc8227
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-28 18:02:06 +0200

    add LinePlay excersize

---
+++ week-03/day-02/W03D02_workshop/src/LinePlay.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+
+  public static void mainDraw(Graphics graphics){
+    // reproduce this:
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/line-play/r1.png]
+    
+    for (int i=0; i < 300; i+=30) {
+      graphics.setColor(new Color(255,0,0,150));
+      graphics.drawLine(0+i, 0,300, 0+i);
+      graphics.setColor(new Color(10,10,100,150));
+      graphics.drawLine(0, 0+i,0+i, 300);
+    }
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
