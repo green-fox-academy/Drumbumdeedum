3c10393347000ce614d7ce64d4d4cbf8873708a3
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-28 16:18:07 +0200

    add PurpleSteps3D excersize

---
+++ week-03/day-02/W03D02_workshop/src/PurpleSteps3D.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3D {
+
+  public static void mainDraw(Graphics graphics){
+
+    for (int i=0; i < 6; i++) {
+      purpleBox(i+((i-1)*10), i*10, graphics);
+    }
+  }
+
+
+  public static void purpleBox (int input1, int input2, Graphics graphics) {
+    graphics.setColor(new Color(75,0,130, 150));
+    graphics.fillRect(input1, input1, input2 , input2);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
