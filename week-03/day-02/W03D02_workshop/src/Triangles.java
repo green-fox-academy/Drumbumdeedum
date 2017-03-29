ce9594e3ea15ebe713a762fca5015119ee6eb9d2
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-29 22:28:00 +0200

    add Triangles excersize

---
+++ week-03/day-02/W03D02_workshop/src/Triangles.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+
+public class Triangles {
+
+  public static void mainDraw(Graphics graphics){
+
+    // Where should the top of the big triangle be?
+    int centerX = 150;
+    int centerY = 30;
+
+    // What should the length of the small triangles sides be?
+    int triangleSide = 20;
+
+    // How many rows of small triangles should the big triangle have?
+    int triangleRows = 10;
+
+    int sideHalf = triangleSide /2;
+    Double sideDouble = triangleSide*Math.sqrt(3)/2;
+    int triangleHeight = sideDouble.intValue();
+
+    for (int i = 1; i < triangleRows+1; i++) {
+      graphics.drawLine(centerX+sideHalf*i, centerY+triangleHeight*i, centerX-sideHalf*i,centerY+triangleHeight*i);
+      graphics.drawLine(centerX-sideHalf*(i-1), centerY+triangleHeight*(i-1), centerX+sideHalf*triangleRows-triangleSide*(i-1),centerY+triangleHeight*triangleRows);
+      graphics.drawLine(centerX+sideHalf*(i-1), centerY+triangleHeight*(i-1), centerX-sideHalf*triangleRows+triangleSide*(i-1),centerY+triangleHeight*triangleRows);
+    }
+  }
+  
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 320));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
