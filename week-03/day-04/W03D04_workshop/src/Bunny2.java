185cc77fcd18dc1e0be9a5677ddf3e382c2df1c3
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-30 12:41:44 +0200

    add Bunny2 excersize...stupid bunnies

--- week-03/day-04/W03D04_workshop/src/Bunny2.java
+++ week-03/day-04/W03D04_workshop/src/Bunny2.java
@@ -1,16 +1,22 @@
-public class Bunny1 {
+public class Bunny2 {
   public static void main(String[] args) {
 
-    System.out.println(countBunnyEars(124));
+    System.out.println(countBunnyEars(12));
 
   }
 
   public static int countBunnyEars (int bunnies) {
+
     if (bunnies == 1) {
-      return bunnies + 1;
+      return bunnies+2;
+    }
+    else if (bunnies % 2 == 0) {
+      int ears = 2;
+      ears = ears + countBunnyEars(bunnies-1);
+      return ears;
     }
     else {
-      int ears = 2;
+      int ears = 3;
       ears = ears + countBunnyEars(bunnies-1);
       return ears;
     }
