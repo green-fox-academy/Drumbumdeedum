aa2e85340332074fa6bae4c5e1c702330057ba98
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-30 22:26:35 +0200

    update Counter excersize

--- week-03/day-04/W03D04_workshop/src/Counter.java
+++ week-03/day-04/W03D04_workshop/src/Counter.java
@@ -1,16 +1,14 @@
 public class Counter {
-  public static void main(String[] args) {
 
-    countDown(6);
+  public static void main(String[] args) {
+    System.out.println(countDown(6));
   }
-  public static int countDown(int topNumber) {
-      if (topNumber == 0) {
-        System.out.println(0);
-      }
-      else {
-        System.out.println(topNumber);
-        countDown(topNumber-1);
-      }
-    return(topNumber);
+
+  public static String countDown(int topNumber) {
+    if (topNumber == 1) {
+      return "" + 1;
+    } else {
+      return "" + topNumber + "\n" + countDown(topNumber - 1);
+    }
   }
 }
