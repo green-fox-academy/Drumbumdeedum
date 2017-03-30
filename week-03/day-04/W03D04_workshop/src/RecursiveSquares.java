b11b13bf57fa808ceb4dc0b85185a18980b982c1
Daniel Szalay <sza.dan.uj@gmail.com>
2017-03-30 19:50:39 +0200

    add finished RecursiveSquares, add RecursiveHexagon attempts

--- week-03/day-04/W03D04_workshop/src/RecursiveSquares.java
+++ week-03/day-04/W03D04_workshop/src/RecursiveSquares.java
@@ -1,5 +1,4 @@
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -7,32 +6,32 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class RecursiveSquares {
-    public static void mainDraw(Graphics graphics){
-      graphics.setColor(new Color(255,255,0,255));
-      graphics.fillRect(0, 0, 600, 600);
-      graphics.setColor(new Color(0,0,0,255));
-      graphics.drawRect(0, 0, 600, 600);
+    public static void mainDraw(Graphics g){
+      g.setColor(new Color(255,255,0,255));
+      g.fillRect(0, 0, 600, 600);
+      g.setColor(new Color(0,0,0,255));
+      g.drawRect(0, 0, 600, 600);
 
 
-      fractals(0,0,600, graphics);
+      fractalize(0,0,600, g);
     }
 
-    public static void lines (int x, int y, int maxLine, Graphics graphics) {
-      graphics.drawLine(x + maxLine/3, y, x + maxLine/3, y + maxLine);
-      graphics.drawLine(x + maxLine/3*2, y,x + maxLine/3*2, y + maxLine);
-      graphics.drawLine(x + 0,y + maxLine/3, x + maxLine, y + maxLine/3);
-      graphics.drawLine(x + 0,y + maxLine/3*2, x + maxLine, y + maxLine/3*2);
+    public static void lines (int x, int y, int maxLine, Graphics g) {
+      g.drawLine(x + maxLine/3, y, x + maxLine/3, y + maxLine);
+      g.drawLine(x + maxLine/3*2, y,x + maxLine/3*2, y + maxLine);
+      g.drawLine(x + 0,y + maxLine/3, x + maxLine, y + maxLine/3);
+      g.drawLine(x + 0,y + maxLine/3*2, x + maxLine, y + maxLine/3*2);
     }
 
-    public static void fractals(int x, int y, int maxLine, Graphics graphics) {
+    public static void fractalize(int x, int y, int maxLine, Graphics g) {
       if (maxLine < 1) {
         return;
       } else {
-        lines(x, y, maxLine, graphics);
-        fractals(x + (maxLine / 3), y, maxLine / 3, graphics);
-        fractals(x, y + (maxLine / 3), maxLine / 3, graphics);
-        fractals(x + ((maxLine / 3) * 2), y + (maxLine / 3), maxLine / 3, graphics);
-        fractals(x + (maxLine / 3), y + ((maxLine / 3) * 2), maxLine / 3, graphics);
+        lines(x, y, maxLine, g);
+        fractalize(x + (maxLine / 3), y, maxLine / 3, g);
+        fractalize(x, y + (maxLine / 3), maxLine / 3, g);
+        fractalize(x + ((maxLine / 3) * 2), y + (maxLine / 3), maxLine / 3, g);
+        fractalize(x + (maxLine / 3), y + ((maxLine / 3) * 2), maxLine / 3, g);
       }
     }
 
