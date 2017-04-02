2ca396e6fb109777dba271711600d8f260177a98
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-02 17:21:11 +0200

    start W04 folder, modify recursive tree to recursive nightforest

--- week-03/day-05/W03D05_workshop/src/RecursiveTree.java
+++ week-03/day-05/W03D05_workshop/src/RecursiveTree.java
@@ -5,11 +5,35 @@ import java.awt.*;
 public class RecursiveTree {
 
   public static void mainDraw(Graphics g) {
-    drawTree(7,550, 700, -90, 35, 100, 17, 70, 50,30, g);
+    drawTree(7,0, 700, -90, 35, 100, 17, 70, 50,30, g);
+    drawTree(7,75, 700, -90, 35, 50, 17, 70, 50,30, g);
+    drawTree(7,150, 700, -90, 35, 70, 17, 70, 50,30, g);
+    drawTree(7,225, 700, -90, 35, 90, 17, 70, 50,30, g);
+    drawTree(7,300, 700, -90, 35, 40, 17, 70, 50,30, g);
+    drawTree(7,370, 700, -90, 35, 120, 17, 70, 50,30, g);
+    drawTree(7,450, 700, -90, 35, 60, 17, 70, 50,30, g);
+    drawTree(7,525, 700, -90, 35, 50, 17, 70, 50,30, g);
+    drawTree(7,600, 700, -90, 35, 80, 17, 70, 50,30, g);
+    drawTree(7,675, 700, -90, 35, 100, 17, 70, 50,30, g);
+    drawTree(7,750, 700, -90, 35, 40, 17, 70, 50,30, g);
+    drawTree(7,820, 700, -90, 35, 60, 17, 70, 50,30, g);
+    drawTree(7,905, 700, -90, 35, 130, 17, 70, 50,30, g);
+    drawTree(7,970, 700, -90, 35, 80, 17, 70, 50,30, g);
+    drawTree(7,1005, 700, -90, 35, 100, 17, 70, 50,30, g);
+
     g.setColor(new Color(102,170,85));
-    g.fillRect(0,700,1000,100);
-    g.setColor(new Color(255,255,0));
+    g.fillRect(0,700,2000,300);
+    g.setColor(new Color(255,255,148));
     g.fillOval(20,20,200,200);
+    g.setColor(new Color(31,32,91));
+    g.fillOval(60,40,200,200);
+
+    for (int i=0; i < 50; i++) {
+      drawStar((int)(Math.random() * 850 + 150), (int)(Math.random() * 240 + 0),10, g);
+    }
+
+
+
   }
 
   public static void drawTree(int depth, int startX, int startY, double angle, double angleAdjust, double length, double lineWidth, int color1, int color2, int color3, Graphics g) {
@@ -30,6 +54,29 @@ public class RecursiveTree {
     }
   }
 
+  public static void drawStar(double centerX, double centerY, double side, Graphics g) {
+
+    double height = (side * Math.sqrt(3d)) / 2d;
+    Polygon triangle = new Polygon();
+
+    triangle.addPoint((int) (centerX - side / 2), (int) (centerY - height / 2));
+    triangle.addPoint((int) (centerX + side / 2), (int) (centerY - height / 2));
+    triangle.addPoint((int) (centerX - 0), (int) (centerY + height / 2));
+
+    g.setColor(new Color(255, 255, 148));
+    g.fillPolygon(triangle);
+
+    Polygon triangle2 = new Polygon();
+
+    triangle2.addPoint((int) (centerX - side / 2), (int) (centerY + height/6));
+    triangle2.addPoint((int) (centerX + side / 2), (int) (centerY + height/6));
+    triangle2.addPoint((int) (centerX - 0), (int) (centerY - height*.9));
+
+    g.setColor(new Color(255, 255, 148));
+    g.fillPolygon(triangle2);
+  }
+
+
 
   //    Don't touch the code below
   public static void main(String[] args) {
@@ -44,7 +91,7 @@ public class RecursiveTree {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
-      this.setBackground(new Color (	126,	192,	238));
+      this.setBackground(new Color (	31,	32,	91));
       mainDraw(graphics);
     }
   }
---
+++ week-04/day-01/W04D01_workshop/src/Trees.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Trees {
+
+  public static void main(String[] args) {
+
+    ArrayList<Map<String, String>> trees = new ArrayList<Map<String, String>>();
+
+    Map<String, String> tree0 = new HashMap<String, String>();
+    tree0.put("type", "chestnut");
+    tree0.put("leaf color", "green");
+    tree0.put("age", "5 years");
+    tree0.put("sex", "female/seed");
+    trees.add(tree0);
+
+
+    Map<String, String> tree1 = new HashMap<String, String>();
+    tree1.put("type", "redwood");
+    tree1.put("leaf color", "green");
+    tree1.put("age", "127 years");
+    tree1.put("sex", "male/pollen");
+    trees.add(tree1);
+
+    Map<String, String> tree2 = new HashMap<String, String>();
+    tree2.put("type", "red oak");
+    tree2.put("leaf color", "red");
+    tree2.put("age", "11 years");
+    tree2.put("sex", "female/seed");
+    trees.add(tree2);
+
+    Map<String, String> tree3 = new HashMap<String, String>();
+    tree3.put("type", "cassia fistula");
+    tree3.put("leaf color", "yellow");
+    tree3.put("age", "3 years");
+    tree3.put("sex", "male/pollen");
+    trees.add(tree3);
+
+    Map<String, String> tree4 = new HashMap<String, String>();
+    tree4.put("type", "birch");
+    tree4.put("leaf color", "green");
+    tree4.put("age", "7 years");
+    tree4.put("sex", "female/seed");
+    trees.add(tree4);
+
+    for (int i=0; i < trees.size(); i++) {
+      System.out.println("tree"+(i+1));
+      System.out.println("\t" + "- type: " + trees.get(i).get("type"));
+      System.out.println("\t" + "- sex: " + trees.get(i).get("sex"));
+      System.out.println("\t" + "- age: " + trees.get(i).get("age"));
+      System.out.println("\t" + "- leaf color: " + trees.get(i).get("leaf color"));
+      System.out.println();
+    }
+
+
+  }
+}
