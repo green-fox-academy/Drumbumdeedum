5b90f8cf3492d9bb95491741508d003b808d38bd
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-03 15:45:40 +0200

    add StudentTeacher files and Counter excersize

---
+++ week-04/day-01/W04D01_workshop/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+  public void learn () {
+    System.out.println("I've learned something!");
+  }
+
+  public void question (Teacher teacher) {
+    teacher.answer();
+  }
+}
