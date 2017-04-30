ae218e4e520d1ee5ea7231a0583f56dab4f4e255
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-30 19:34:41 +0200

    update

--- week-05/RPGGAME/src/ImageLoader.java
+++ week-05/RPGGAME/src/ImageLoader.java
@@ -6,18 +6,19 @@ import javax.imageio.ImageIO;
 public class ImageLoader {
 
   private static ImageLoader instance;
-  public BufferedImage FLOOR, WALL, HERO_UP, HERO_DOWN, HERO_LEFT, HERO_RIGHT, BOSS, SKELETON;
+  public BufferedImage FLOOR, WALL, HERO_UP, HERO_DOWN, HERO_LEFT, HERO_RIGHT, BOSS, SKELETON, HEART, KISCICA;
 
   private ImageLoader() {
     try {
       FLOOR = ImageIO.read(new File("assets/floor.png"));
       WALL = ImageIO.read(new File("assets/wall.png"));
-      HERO_UP = ImageIO.read(new File("assets/hero-up.png"));
-      HERO_DOWN = ImageIO.read(new File("assets/nyanyi.png"));
-      HERO_LEFT = ImageIO.read(new File("assets/hero-left.png"));
-      HERO_RIGHT = ImageIO.read(new File("assets/hero-right.png"));
-      BOSS = ImageIO.read(new File("assets/kiscica.png"));
+      HERO_UP = ImageIO.read(new File(/*"assets/hero-up.png"*/ "assets/nyanyi.png"));
+      HERO_DOWN = ImageIO.read(new File(/*"assets/hero-down.png"*/"assets/nyanyi.png"));
+      HERO_LEFT = ImageIO.read(new File(/*"assets/hero-left.png"*/"assets/nyanyi.png"));
+      HERO_RIGHT = ImageIO.read(new File(/*"assets/hero-right.png"*/"assets/nyanyi.png"));
+      BOSS = ImageIO.read(new File("assets/boss.png"));
       SKELETON = ImageIO.read(new File("assets/skeleton.png"));
+      HEART = ImageIO.read(new File("assets/heart.png"));
     } catch (IOException e) {
       e.printStackTrace();
     }
