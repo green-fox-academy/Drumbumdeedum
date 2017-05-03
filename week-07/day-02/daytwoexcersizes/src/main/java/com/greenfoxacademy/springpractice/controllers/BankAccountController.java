e047546b2f75096aa6ba4610f3ae53aea51d2f4d
Daniel Szalay <sza.dan.uj@gmail.com>
2017-05-03 12:43:00 +0200

    update excersize1

--- week-07/day-02/daytwoexcersizes/src/main/java/com/greenfoxacademy/springpractice/controllers/BankAccountController.java
+++ week-07/day-02/daytwoexcersizes/src/main/java/com/greenfoxacademy/springpractice/controllers/BankAccountController.java
@@ -11,15 +11,18 @@ public class BankAccountController {
 
   BankAccount newAccount1;
   BankAccount newAccount2;
+  BankAccount newAccount3;
   ArrayList<BankAccount> accounts = new ArrayList<>();
 
   @RequestMapping("/Excersize1")
   public String initializeBankAccount(Model model) {
     newAccount1 = new BankAccount("Simba", "lion", "2000");
     newAccount2 = new BankAccount("Scar", "lion", "1000");
+    newAccount3 = new BankAccount("Bagheera", "panther", "1800");
     accounts.add(newAccount1);
     accounts.add(newAccount2);
+    accounts.add(newAccount3);
     model.addAttribute("accounts", accounts);
-    return "showFields";
+    return "showBankAccount";
   }
 }
