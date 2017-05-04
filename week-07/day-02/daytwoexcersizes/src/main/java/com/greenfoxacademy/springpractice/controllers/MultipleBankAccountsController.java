f35dd73f2228f50ccf2eab366ae427c4ab3aff74
Daniel Szalay <sza.dan.uj@gmail.com>
2017-05-04 09:22:43 +0200

    add solution for excersize 11

--- week-07/day-02/daytwoexcersizes/src/main/java/com/greenfoxacademy/springpractice/controllers/MultipleBankAccountsController.java
+++ week-07/day-02/daytwoexcersizes/src/main/java/com/greenfoxacademy/springpractice/controllers/MultipleBankAccountsController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MultipleBankAccountsController {
@@ -13,21 +14,37 @@ public class MultipleBankAccountsController {
 
   @RequestMapping("/Excersise5")
   public String initializeBankAccount(Model model) {
-    accounts.add(new BankAccount("Simba", "lion", 2000, false, false));
-    accounts.add(new BankAccount("Scar", "lion", 1400, false, true));
-    accounts.add(new BankAccount("Mufasa", "lion", 2400, true, false));
-    accounts.add(new BankAccount("Rafiki", "mandrill", 1, false, false));
-    accounts.add(new BankAccount("Pumba", "warthog", 0, false, false));
-    accounts.add(new BankAccount("Timon", "meerkat", 0, false, false));
+    if (accounts.size() == 0) {
+      accounts.add(new BankAccount("Simba", "lion", 2000, false, false));
+      accounts.add(new BankAccount("Scar", "lion", 1400, false, true));
+      accounts.add(new BankAccount("Mufasa", "lion", 2400, true, false));
+      accounts.add(new BankAccount("Rafiki", "mandrill", 1, false, false));
+      accounts.add(new BankAccount("Pumba", "warthog", 0, false, false));
+      accounts.add(new BankAccount("Timon", "meerkat", 0, false, false));
+    }
     model.addAttribute("accounts", accounts);
     return "showBankAccount";
   }
 
   @RequestMapping("/addZebras")
   public String addZebras(int index) {
-    System.out.println(index);
     accounts.get(index).setBalance();
-    System.out.println(accounts.get(index).getBalance());
+    return "redirect:/Excersise5";
+  }
+
+  @RequestMapping("/addAccount")
+  public String addAccount(String name, String type, String balance, String guy) {
+    boolean badGuy = false;
+    int intBalance = Integer.parseInt(balance);
+
+    if (guy.toLowerCase().contains("yes")) {
+      badGuy = true;
+    }
+    if (guy.toLowerCase().contains("no")) {
+      badGuy = false;
+    }
+
+    accounts.add(new BankAccount(name, type, intBalance, false, badGuy));
     return "redirect:/Excersise5";
   }
 }
