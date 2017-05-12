55686fbe1f7c0a7cd3e527636d3ed590cde0f33d
Daniel Szalay <sza.dan.uj@gmail.com>
2017-05-12 13:41:20 +0200

    fix upvote/downvite

--- RedditApp/src/main/java/com/greenfox/dszalay/RedditApp/controllers/PostController.java
+++ RedditApp/src/main/java/com/greenfox/dszalay/RedditApp/controllers/PostController.java
@@ -5,6 +5,7 @@ import com.greenfox.dszalay.RedditApp.models.PostList;
 import com.greenfox.dszalay.RedditApp.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,21 @@ public class PostController {
   @PostMapping("/post")
   public void addPost(@RequestBody Post post) {
     repository.save(post);
+  }
+
+  @PostMapping("/post/{id}/upvote")
+  public Post upVote(@PathVariable long id) {
+    Post post = repository.findOne(id);
+    post.setScore(post.getScore()+1);
+    repository.save(post);
+    return post;
+  }
+
+  @PostMapping("/post/{id}/downvote")
+  public Post downVote(@PathVariable long id) {
+    Post post = repository.findOne(id);
+    post.setScore(post.getScore()-1);
+    repository.save(post);
+    return post;
   }
 }
