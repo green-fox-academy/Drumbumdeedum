b23749b5cc2dc6c42d78639d94166bf71fc95807
Daniel Szalay <sza.dan.uj@gmail.com>
2017-07-10 10:14:53 +0200

    shorten description

--- final-exam-jumpin/src/ExerciseThirteen.java
+++ final-exam-jumpin/src/ExerciseThirteen.java
@@ -1,17 +1,10 @@
-/*13.
+/*  13.
 
-  Create a function that takes a list of strings and transforms it like:
-  [
-    "abc",
-    "ijk",
-    "xyz"
-  ]
-  becomes:
-  [
-    "aix",
-    "bjy",
-    "ckz"
-  ]*/
+    Create a function that takes a list of strings and transforms it like:
+    ["abc", "ijk", "xyz"]
+    becomes:
+    ["aix", "bjy", "ckz"]
+*/
 
 import java.util.ArrayList;
 import java.util.Arrays;
