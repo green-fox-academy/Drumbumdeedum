4d5d90eed290ed742e6e3f71174f59866f4dc8e5
Daniel Szalay <sza.dan.uj@gmail.com>
2017-07-30 16:41:01 +0200

    add bigint factor sum of digits solution

---
+++ AlgorithmDay/src/SumOfFactorialDigits.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+
+public class SumOfFactorialDigits {
+
+  public static void main(String[] args) {
+    System.out.println(factorialBigInt(100));
+    System.out.println(calcDigits(factorialBigInt(100).toString()));
+  }
+
+  public static BigInteger factorialBigInt(int input) {
+    BigInteger fact = BigInteger.valueOf(1);
+    for (int i = 1; i <= input; i++)
+      fact = fact.multiply(BigInteger.valueOf(i));
+    return fact;
+  }
+
+  private static int calcDigits(String s) {
+    int sum = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+      Character c = new Character(s.charAt(i));
+      String z = c.toString();
+      int j = Integer.parseInt(z);
+      sum += j;
+    }
+    return sum;
+  }
+}
