21efa2c88118999b27a24bce7f80596fe24d90a5
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-03 10:18:59 +0200

    add permutations final solution

--- AlgorithmDay/src/PermutationsOfInt.java
+++ AlgorithmDay/src/PermutationsOfInt.java
@@ -7,34 +7,32 @@ public class PermutationsOfInt {
   private static final ArrayList<Long> possibleNumbers = new ArrayList<>(Arrays.asList(0l,1l,2l,3l,4l,5l,6l,7l,8l,9l));
 
   public static void main(String[] args) {
+    ArrayList<String> result = permutation("0123456789");
+    Collections.sort(result);
+    System.out.println(result.get(999999));
+  }
 
-    ArrayList<Long> allNumbers = new ArrayList<>();
+  public static ArrayList<String> permutation(String input) {
+    ArrayList<String> permutations = new ArrayList<>();
+    if (input.length() == 1) {
+      permutations.add(input);
+    } else if (input.length() > 1) {
+      int lastIndex = input.length() - 1;
+      String last = input.substring(lastIndex);
+      String rest = input.substring(0, lastIndex);
+      permutations = merge(permutation(rest), last);
+    }
+    return permutations;
+  }
 
-    while (allNumbers.size() < factorial(possibleNumbers.size())) {
-      long valueToAdd = randomNumber();
-      if(!allNumbers.contains(valueToAdd)) {
-        allNumbers.add(valueToAdd);
-        Collections.sort(allNumbers);
-        System.out.println(allNumbers.size());
+  public static ArrayList<String> merge(ArrayList<String> list, String c) {
+    ArrayList<String> res = new ArrayList<>();
+    for (String s : list) {
+      for (int i = 0; i <= s.length(); ++i) {
+        String ps = new StringBuffer(s).insert(i, c).toString();
+        res.add(ps);
       }
     }
-    System.out.println(allNumbers.get(1000000));
-  }
-
-  public static long factorial(long input) {
-    long result = 1;
-    for (int i = 1; i <= input; i++) {
-      result *= i;
-    }
-    return result;
-  }
-
-  public static Long randomNumber() {
-    Collections.shuffle(possibleNumbers);
-    StringBuilder numberString = new StringBuilder();
-    for (int i = 0; i < possibleNumbers.size(); i++) {
-      numberString.append(possibleNumbers.get(i));
-    }
-    return Long.valueOf(numberString.toString());
+    return res;
   }
 }
